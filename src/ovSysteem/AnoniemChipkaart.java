4dd8025d3e442df20f8c904303d11a6145141433
Hebaayoubi <1218091@student.roc-nijmegen.nl>
2026-04-10 09:23:54 +0200

    .
    ik heb het verbeterd

--- src/ovSysteem/AnoniemChipkaart.java
+++ src/ovSysteem/AnoniemChipkaart.java
@@ -6,7 +6,9 @@ public class AnoniemChipkaart {
 	  /* het besgra wat er op het ov-pas staat*/
 	    private boolean ingecheckt;
 	    /* dat gefet aan of je bent ingechecktt op dit moment true/false*/ 
-	    
+	    private String inchecklocatie; 
+	    /*nieuwe variabele om een locatie om-p te slaan*/ 
+	   
 	    public AnoniemChipkaart(String kaartnummer, double saldo) {
 	    /* je maakt een nieuwe object*/ 	
 	    	/* kaaratnummer dat zou het kaartnummer van de ov zijn*/ 
@@ -16,6 +18,8 @@ public class AnoniemChipkaart {
 	        /* het object krijgt saldo*/ 
 	        this.ingecheckt = false;
 	        /* de nieuw ov-pas is standaard niet ingecheckt dus dat is false */ 
+	        this.inchecklocatie= null; 
+	        /* er is op dti moment geen inchecklocatie*/ 
 	    }
 	    public double Saldo() {
 	        return saldo;
@@ -26,15 +30,19 @@ public class AnoniemChipkaart {
 	        return ingecheckt;
 	        /* geeft terug of je hebt ingechcket */ 
 	    }
+	    public String inchecklocatie() { 
+	    	return inchecklocatie; 
+	    }
 
 	    public void opwaarden(double bedrag) {
 	        saldo += bedrag;
 	        /* verhoogd het saldo*/ 
 	    }
 
-	    public void inchecken(double incheckBedrag) {
+	    public void inchecken(double incheckBedrag , String locatie ) {
 	        saldo +=incheckBedrag ;
-	        ingecheckt = true;
+	        ingecheckt =true;
+	        inchecklocatie =locatie; 
 	        /* je bent ingechecjt dus er gaaat geld van je pas */ 
 	        /* je pas wordt ingecheck gezet*/ 
 	    }
@@ -42,6 +50,7 @@ public class AnoniemChipkaart {
 	    public void uitchecken(double ritprijs) {
 	        saldo -= ritprijs;
 	        ingecheckt = false;
+	        inchecklocatie = null; 
 	        /* er wordt geld van je saldo gehaald*/ 
 	        /* de pas wordt uitgecheckt*/ 
 	    }
